cdc9f4c82fa9cb6e8ef188d34155975ebde08395
Carlos Martinez <neocarlitos@gmail.com>
2019-06-15 11:29:39 +0200

    Add debug info

--- .travis/install.sh
+++ .travis/install.sh
@@ -14,8 +14,17 @@ if [[ $TRAVIS_BUILD_STAGE_NAME == 'Deploy' ]]; then
   virtualenv venv -p python$PYENV_VERSION
   source venv/bin/activate
 fi
+echo "Python version"
+python --version
+
+echo "Pip version"
+pip --version
+
 
 # Horrible hack -- but we should remove pythonql functionality soon anyway...
 pip install pythonql3
+echo "Install dot"
 pip install .
+
+echo "Install test requirements"
 pip install -r requirements-test.txt
